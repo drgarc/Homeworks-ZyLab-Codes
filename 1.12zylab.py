d026f59908ded86cba123af26fd15025ebfb4d36
drgarc <89817604+drgarc@users.noreply.github.com>
2021-09-17 01:55:16 -0500

    Add files via upload

---
+++ 1.12zylab.py
@@ -0,0 +1,8 @@
+#Reading input from user
+user_num = int(input())
+
+#Squaring user_num and storing in user_num_squared
+user_num_squared = user_num * user_num
+
+#Printing the user_num_squared
+print(user_num_squared)
